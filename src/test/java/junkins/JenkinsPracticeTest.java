42a00eeb697f2ac1039214070a0e8d98f0002c22
deepi <deepi@Ashwini>
2025-12-03 15:16:33 +0530

    First commit

---
+++ src/test/java/junkins/JenkinsPracticeTest.java
@@ -0,0 +1,13 @@
+package junkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsPracticeTest {
+	@Test
+	public void jenkisPractice() {
+	
+			Reporter.log("Hii Jenkis",true);
+	}
+
+}
